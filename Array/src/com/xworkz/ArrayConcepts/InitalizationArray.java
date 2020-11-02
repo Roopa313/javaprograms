2cef7b5118e31307a9834129961560ce47790dc6
Roopa313 <72559540+Roopa313@users.noreply.github.com>
2020-11-02 14:28:04 +0530

    Add files via upload

---
+++ Array/src/com/xworkz/ArrayConcepts/InitalizationArray.java
@@ -0,0 +1,23 @@
+package com.xworkz.ArrayConcepts;
+
+import java.util.Arrays;
+
+
+public class InitalizationArray {
+	
+	 public static void main(String[] args) {
+
+	        int[] a = new int[5];//declaration ,Initialization
+
+	        a[0] = 1;
+	        a[1] = 2;
+	        a[2] = 3;
+	        a[3] = 4;
+	        a[4] = 5;
+	        
+
+	        System.out.println(Arrays.toString(a));
+	    }
+	}
+
+
---
+++ Array/src/com/xworkz/ArrayConcepts/IntializationArray2.java
@@ -0,0 +1,18 @@
+package com.xworkz.ArrayConcepts;
+
+import java.util.Arrays;
+
+public class IntializationArray2 {
+	
+	
+	public static void main(String[] args) {
+
+        int[] a = new int[] { 2, 4, 5, 6, 7, 3, 2 };
+
+        System.out.println(Arrays.toString(a));
+    }
+}
+	
+	
+
+
