264a7cca4be42d19aa350b7d82c9737c82371a61
Roopa313 <72559540+Roopa313@users.noreply.github.com>
2020-11-02 14:30:03 +0530

    Add files via upload

---
+++ EncapsulationExamples/src/com/xworkz/Encapsulation/EncapTester.java
@@ -0,0 +1,23 @@
+package com.xworkz.Encapsulation;
+
+public class EncapTester {
+	
+	
+	public static void main(String[] args)
+	{ 
+	//Step 6: Create an object of class Number using the new keyword. 
+	 Number n = new Number(); // Here, n is a reference variable of Number and pointing to the object of class Number. 
+
+	//Step 7: Now call setter method and set value of the variable. 
+	   n.set(6); 
+
+	//Step 8: Call getter method to read the value of variable. 
+	  int num = n.get(); 
+
+	//Step 9: Print the output. 
+	  System.out.println(num); 
+	  } 
+	}
+
+
+
---
+++ EncapsulationExamples/src/com/xworkz/Encapsulation/Number.java
@@ -0,0 +1,20 @@
+package com.xworkz.Encapsulation;
+
+public class Number {
+	
+	//Step 1: Create a package named 'encapsulation' and make a class 'Number'
+	
+	//Step 2: Declare field/variable as private. 
+	   private int y; 
+
+//	Step 3: Create a public getter method for private variable. 
+	public int get() { 
+	    return y; 
+	} 
+	//Step 4: Create a public setter method for private variable and declare the parameter. 
+	public void set(int y) { 
+	   this.y = y; 
+	 } 
+	} 
+	//Step4: Create another class EncapTest. 
+	
