de54fe6340550539c10874069574bf86add63d0b
Roopa313 <72559540+Roopa313@users.noreply.github.com>
2020-11-02 14:38:38 +0530

    Add files via upload

---
+++ WrapperClass/src/com/xworkz/WrapperClass/WrapperExample.java
@@ -0,0 +1,17 @@
+package com.xworkz.WrapperClass;
+
+public class WrapperExample {
+	
+	public static void main(String[] args) {
+		
+		
+	    Integer myInt = 5;
+	    Double myDouble = 5.99;
+	    Character myChar = 'A';
+	    System.out.println(myInt);
+	    System.out.println(myDouble);
+	    System.out.println(myChar);
+	  }
+	}
+
+
