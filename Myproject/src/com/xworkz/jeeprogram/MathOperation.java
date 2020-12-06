b8292d0962cd9309db89af101ec2a9f17e23e9aa
Roopa313 <72559540+Roopa313@users.noreply.github.com>
2020-12-06 07:47:02 +0530

    Add files via upload

---
+++ Myproject/src/com/xworkz/jeeprogram/MathOperation.java
@@ -0,0 +1,12 @@
+package com.xworkz.jeeprogram;
+
+public interface  MathOperation {
+	
+	int addition(int a,int b);
+	int subtraction(int a, int b);
+	int multiplication(int a, int b);
+	int division(int a, int b);
+	int modulus(int a, int b);
+}
+
+
---
+++ Myproject/src/com/xworkz/jeeprogram/MathOperationsImpl.java
@@ -0,0 +1,38 @@
+package com.xworkz.jeeprogram;
+
+public class MathOperationsImpl implements MathOperation {
+
+	@Override
+	public int addition(int a, int b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int subtraction(int a, int b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int multiplication(int a, int b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int division(int a, int b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int modulus(int a, int b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	@Override
+	
+	
+	
